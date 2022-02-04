1e4b24d78ceb1ec7601e0eea62c4b439ef10b80f
saketh patchava <64890181+sakethpatchava@users.noreply.github.com>
2022-02-04 09:56:15 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,5 +1,3 @@
-from turtle import Turtle
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -24,6 +22,13 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
